3a7cc625edf4f85cce6975b6f74245ead767ff51
cvon117 <125157011+Craigien@users.noreply.github.com>
2023-06-08 17:44:16 -0400

    Adding functionality to check which answer user selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,33 @@
 // Global variables
 
 var welcomeMessageEl = document.querySelector("#welcome");
-var questionsEl = document.querySelector("#questions");
+var questionsFormEl = document.querySelector("#questions");
 var highScoresEl = document.querySelector("#highscores");
 var initialsAndScoreEl = document.querySelector("#initials-and-score");
 var timerEl = document.querySelector("#timer");
 var startButtonEl = document.querySelector("#start-button");
 
 var questions = {
-    0: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"],
-    1: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"],
-    2: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"],
-    3: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"],
-    4: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"]
-}
+    0: ["Text1", "Answer1", "Answer2", "Answer3", "Answer4"],
+    1: ["Text2", "Answer1", "Answer2", "Answer3", "Answer4"],
+    2: ["Text3", "Answer1", "Answer2", "Answer3", "Answer4"],
+    3: ["Text4", "Answer1", "Answer2", "Answer3", "Answer4"],
+    4: ["Text5", "Answer1", "Answer2", "Answer3", "Answer4"]
+};
+
+var answers = ["answer1", "answer1", "answer1", "answer1", "answer1"];
+
+var options = {
+    1: document.querySelector("#option1"),
+    2: document.querySelector("#option2"),
+    3: document.querySelector("#option3"),
+    4: document.querySelector("#option4")
+};
 
 var timer;
 var timeLeft = 0;
 var win = false;
+var score;
 
 function init()
 {
@@ -51,38 +61,74 @@ function startTimer()
 function answerSelection()
 {
     // Check answer
+
+
+
+    /*
+    options[1].addEventListener("click", function()
+    {
+        return true;
+    });
+
+    options[2].addEventListener("click", function()
+    {
+        return false;
+    });
+
+    options[3].addEventListener("click", function()
+    {
+        return false;
+    });
+
+    options[4].addEventListener("click", function()
+    {
+        return false;
+    });
+    */
 }
 
 function clearQuestion()
 {
-    questionsEl.children[0].textContent = "";
+    questionsFormEl.children[0].textContent = "";
 
     do
     {
-        questionsEl.children[1].removeChild(questionsEl.children[1].firstChild)
+        questionsFormEl.children[1].removeChild(questionsFormEl.children[1].firstChild)
     }
-    while (questionsEl.children[1].firstChild);
+    while (questionsFormEl.children[1].firstChild);
 }
 
 function displayQuestions()
 {
     for (var i = 0; i < 1; i++)
     {
-        questionsEl.children[0].textContent = questions[i][0];
+        // Targets first div
+        questionsFormEl.children[0].textContent = questions[i][0];
 
         for (var j = 1; j < 5; j++)
         {
-            var answer0 = document.createElement("li");
-            answer0.textContent = questions[i][j];
+            console.log(options[j]);
 
-            questionsEl.children[1].append(answer0.textContent);
+            var answer = options[j];
+
+            answer.textContent = questions[i][j];
         }
 
         answerSelection();
 
-        if (answerSelection == true)
+        if (answerSelection)
         {
             // Add score
+            score++;
+            console.log(score);
+            clearQuestion();
+        }
+
+        if (!answerSelection)
+        {
+            // Subtract time
+            timeLeft--;
+            console.log(score);
             clearQuestion();
         }
     }
@@ -91,13 +137,15 @@ function displayQuestions()
 function startQuiz()
 {
     win = false;
+    score = 0;
     timeLeft = 10;
     timerEl.textContent = timeLeft;
     welcomeMessageEl.hidden = true;
+
     startTimer();
     displayQuestions();
 }
 
 startButtonEl.addEventListener("click", startQuiz);
 
-init();
+// init();
