af0447a46a0ec9b7f6b770fa5b1b570e4fceb552
cvon117 <125157011+Craigien@users.noreply.github.com>
2023-06-07 20:05:39 -0400

    Added additional functions to JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ var initialsAndScoreEl = document.querySelector("#initials-and-score");
 var timerEl = document.querySelector("#timer");
 var startButtonEl = document.querySelector("#start-button");
 
+var questions = {
+    0: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"],
+    1: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"],
+    2: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"],
+    3: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"],
+    4: ["Text", "Answer1", "Answer2", "Answer3", "Answer4"]
+}
+
 var timer;
 var timeLeft = 0;
 var win = false;
@@ -14,7 +22,6 @@ var win = false;
 function init()
 {
     
-
 }
 
 function startTimer()
@@ -23,22 +30,62 @@ function startTimer()
     {
         timeLeft--;
         timerEl.textContent = timeLeft;
-
+        /*
         if (timeLeft >= 0)
         {
             if (complete && timeLeft > 0)
             {
                 clearInterval(timer);
-                finishedQuiz();
+                // finishedQuiz();
             }
         }
-
+        */
         if (timeLeft === 0)
         {
             clearInterval(timer);
-            noTimeLeft();
+            // noTimeLeft();
         }
     }, 1000);
+}
+
+function answerSelection()
+{
+    // Check answer
+}
+
+function clearQuestion()
+{
+    questionsEl.children[0].textContent = "";
+
+    do
+    {
+        questionsEl.children[1].removeChild(questionsEl.children[1].firstChild)
+    }
+    while (questionsEl.children[1].firstChild);
+}
+
+function displayQuestions()
+{
+    for (var i = 0; i < 1; i++)
+    {
+        questionsEl.children[0].textContent = questions[i][0];
+
+        for (var j = 1; j < 5; j++)
+        {
+            var answer0 = document.createElement("li");
+            answer0.textContent = questions[i][j];
+
+            questionsEl.children[1].append(answer0.textContent);
+        }
+
+        answerSelection();
+
+        if (answerSelection == true)
+        {
+            // Add score
+            clearQuestion();
+        }
+    }
 }
 
 function startQuiz()
@@ -47,6 +94,7 @@ function startQuiz()
     timeLeft = 10;
     timerEl.textContent = timeLeft;
     startTimer();
+    displayQuestions();
 }
 
 startButtonEl.addEventListener("click", startQuiz);
